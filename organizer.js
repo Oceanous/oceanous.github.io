9914ca9b868822cdddc00d0e3f24c9dd83b0a5d6
Lam Nguyen <lamlnguyen@yahoo.com>
2022-02-12 15:45:59 -0800

    adds basic api functionality

--- organizer.js
+++ organizer.js
@@ -1,12 +1,35 @@
-function catFact() {
-    fetch('https://catfact.ninja/fact')
-  .then(response => response.json())
-  .then(data => console.log(data));
+async function catFact() {
+  let response = await fetch('https://catfact.ninja/fact');
+  let data = await response.text();
+  console.log(data);
 }
 
-const token = 'OTQxMTU3NzU5ODIxNDE0NDAw.YgR3dw.6MVkLFXKtIJvZnYzljvpEoPRbIM';
-fetch('https://discord.com/api/users/@me', {
-  headers: {
-    Authorization: `Bot ${token}`
+function updateBossOptions() {
+  var x = document.getElementById("boss1");
+  var option = new Option('Option Text','Option Value');
+  x.options.add(option);
+}
+
+async function getAvatar(userId) {
+  let data = {userId:`${userId}`}
+  console.log(userId)
+  console.log(JSON.stringify(data))
+  let response = await fetch(`https://2xtju1ecpi.execute-api.us-west-1.amazonaws.com/DiscordProxy`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  });
+  let image = document.getElementById("testAvi");
+  let resp = await response.json();
+  console.log(resp)
+  image.src = `https://cdn.discordapp.com/avatars/${userId}/${resp.avatar}`;
+}
+
+function entered(e, ele) {
+  var code = (e.keyCode ? e.keyCode : e.which);
+  if(code == 13) { //Enter keycode
+    getAvatar(ele.value)
   }
-}).then(response => response.json()).then(data => console.log(data));
+}
